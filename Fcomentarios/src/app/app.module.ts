b8c6949fc81c485243010177f66e43263054d234
Rodrigo Martinez <58400562+Roderick1325@users.noreply.github.com>
2020-12-11 23:09:16 -0600

    agregando archivos a "component/agregarEditarComentario"

--- Fcomentarios/src/app/app.module.ts
+++ Fcomentarios/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AgregarEditarComentarioComponent } from './components/agregar-editar-comentario/agregar-editar-comentario.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AgregarEditarComentarioComponent
   ],
   imports: [
     BrowserModule,
