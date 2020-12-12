f7cd97c1b1cf3c0241475b6d5960855f7a7d0353
Rodrigo Martinez <58400562+Roderick1325@users.noreply.github.com>
2020-12-11 23:14:10 -0600

    Agregado verComentario "components/verComentario"

--- Fcomentarios/src/app/app.module.ts
+++ Fcomentarios/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { AgregarEditarComentarioComponent } from './components/agregar-editar-comentario/agregar-editar-comentario.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { ListComentariosComponent } from './components/list-comentarios/list-comentarios.component';
+import { VerComentarioComponent } from './components/ver-comentario/ver-comentario.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AgregarEditarComentarioComponent,
     NavbarComponent,
-    ListComentariosComponent
+    ListComentariosComponent,
+    VerComentarioComponent
   ],
   imports: [
     BrowserModule,
