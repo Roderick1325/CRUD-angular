9907e3c1bb194a7adf0e94bc786f5de65934262d
Rodrigo Martinez <58400562+Roderick1325@users.noreply.github.com>
2020-12-13 12:39:26 -0600

    Modificaciones

--- Fcomentarios/src/app/app-routing.module.ts
+++ Fcomentarios/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AgregarEditarComentarioComponent } from './components/agregar-editar-comentario/agregar-editar-comentario.component';
+import { ListComentariosComponent } from './components/list-comentarios/list-comentarios.component';
+import { VerComentarioComponent } from './components/ver-comentario/ver-comentario.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'agregar', component: AgregarEditarComentarioComponent},
+  {path: 'editar/:id', component: AgregarEditarComentarioComponent},
+  {path: 'ver/:id', component: VerComentarioComponent},
+  {path: '', component: ListComentariosComponent, pathMatch: 'full'},
+  {path: '**', redirectTo:'/'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
