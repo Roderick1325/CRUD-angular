805ddf15a206a98cd0059a7aa2178f91e196451b
Rodrigo Martinez <58400562+Roderick1325@users.noreply.github.com>
2020-12-13 12:41:36 -0600

    Modificaciones

--- Fcomentarios/src/app/components/list-comentarios/list-comentarios.component.ts
+++ Fcomentarios/src/app/components/list-comentarios/list-comentarios.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Comentario } from 'src/app/models/comentario';
+import { ComentarioService } from 'src/app/services/comentario.service';
 
 @Component({
   selector: 'app-list-comentarios',
@@ -6,10 +8,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./list-comentarios.component.css']
 })
 export class ListComentariosComponent implements OnInit {
-
-  constructor() { }
+  listComentarios: Comentario[];
+  loading = false;
+  constructor(private comentarioServcie: ComentarioService) { }
 
   ngOnInit(): void {
+    this.cargarComentario();
   }
+cargarComentario(){
+  this.loading = true;
+  this.comentarioServcie.getListComentarios().subscribe(data =>{
+    this.loading = false;
+    this.listComentarios = data;
+  })
+}
+
+delete(id: number){
+  this.loading = true;
+  this.comentarioServcie.deleteComentario(id).subscribe(data =>{
+    this.cargarComentario()
+    this.loading = false;
+  })
+}
 
 }
+
