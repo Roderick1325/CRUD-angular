8bc0a3f0cb91dbfe61b8b2acc58dbe7d9140bfbb
Rodrigo Martinez <58400562+Roderick1325@users.noreply.github.com>
2020-12-13 12:46:35 -0600

    Modificaciones

--- Fcomentarios/src/app/components/ver-comentario/ver-comentario.component.ts
+++ Fcomentarios/src/app/components/ver-comentario/ver-comentario.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Comentario } from 'src/app/models/comentario';
+import { ComentarioService } from 'src/app/services/comentario.service';
 
 @Component({
   selector: 'app-ver-comentario',
@@ -6,10 +9,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ver-comentario.component.css']
 })
 export class VerComentarioComponent implements OnInit {
+  loading = false;
+  comentario: Comentario;
+  idcomentario: number;
 
-  constructor() { }
-
+  constructor(private comenatrioService: ComentarioService, private route: ActivatedRoute) { 
+    this.idcomentario = +this.route.snapshot.paramMap.get('id');
+  }
   ngOnInit(): void {
+    this.cargarComentario();
+  }
+
+  cargarComentario(){
+    this.loading = true;
+    this.comenatrioService.cargarComentario(this.idcomentario).subscribe(data => {
+      this.loading = false;
+      this.comentario = data;
+
+    })
   }
 
 }
